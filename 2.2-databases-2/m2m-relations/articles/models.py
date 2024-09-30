0e5e60b9cead112f8b5029713fd1085d5161fd7c
magic.kch <magic.kch@ya.ru>
2024-09-30 13:16:15 +0300

    Задание m2m-relations

--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -7,10 +7,40 @@ class Article(models.Model):
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
     image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
+    tags = models.ManyToManyField('Tag', through='Scope', related_name='articles', verbose_name='Раздел')
 
     class Meta:
         verbose_name = 'Статья'
         verbose_name_plural = 'Статьи'
+        ordering = ['-published_at']
 
     def __str__(self):
         return self.title
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=256, verbose_name='Название')
+
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+
+    def __str__(self):
+        return self.name
+
+
+class Scope(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='scopes', verbose_name='Статья')
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, related_name='scopes', verbose_name='Раздел')
+    is_main = models.BooleanField(verbose_name='Основа',)
+
+    class Meta:
+        verbose_name = 'Тематика статьи'
+        verbose_name_plural = 'Тематика статьи'
+        ordering = ['-is_main']
+        constraints = [
+            models.UniqueConstraint(fields=['article', 'tag'], name='unique_article_tag')
+        ]
+
+    def __str__(self):
+        return f'{self.article} - {self.tag}'
